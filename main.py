64743fba4fcc2cd537d74b4c2aeb84dcd65b0318
clohertyg <143036937+clohertyg@users.noreply.github.com>
2023-09-21 08:01:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from PyPDF2 import PdfReader
 
-reader = PdfReader('DOT_PerformancePlan.pdf')
+def findKeyword(document, pageNum, targetWord):
+    reader = PdfReader(document)
+    page = reader.pages[pageNum]
+    pageExtract = page.extract_text()
+    pageOutput = pageExtract.split()
+    pageOutputLower = [x.lower() for x in pageOutput]
+    print(pageOutputLower)
+    if targetWord in pageOutputLower:
+        wordCount = pageOutputLower.count(targetWord)
+        print(f"yes, {targetWord} is in this page {wordCount} time(s)")
+    else:
+        print("no")
+    return
 
-page = reader.pages[0]
+findKeyword('DOT_PerformancePlan.pdf',20,'safety')
 
-text = page.extract_text()
-print(text)
+# need to find some ranking system for word counts
+# highest word count is most relevant
